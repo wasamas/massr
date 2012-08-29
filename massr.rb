ba230c2e9c880428ad7b27728650d5c1c45d4b73
Echos <echo.orz@gmail.com>
2012-08-30 01:05:06 +0900

    heroku用のmongoHQ接続ロジックの変更２

--- massr.rb
+++ massr.rb
@@ -37,11 +37,9 @@ module Massr
 		configure :production do
          @auth_twitter  = {:id => ENV['TWITTER_CONSUMER_ID'], :secret => ENV['TWITTER_CONSUMER_SECRET']}
 
-			p uri =  URI.parse(ENV['MONGOHQ_URL'])
-
-			mongo_connection = Mongo::Connection.from_uri( uri )
-			mongo_db = mongo_connection.db(uri.path.gsub(/^\//, ''))
-			mongo_db.authenticate(uri.user, uri.password)
+			uri = URI.parse(ENV['MONGOHQ_URL'])
+			MongoMapper.connection = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+			MongoMapper.database = uri.path.gsub(/^\//, '')
 
 		end
 
