744dc89ec81f59bd0e7a5ae0240d2857b3d42ed0
Echos <echo.orz@gmail.com>
2012-09-13 03:24:35 +0900

    テストケースの修正

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -4,58 +4,44 @@ require 'massr/models/entry'
 require 'massr/models/user'
 require 'massr/models/like'
 
-def prototype_entry(no)
-	[
-		{
-			# 元エントリ用
-			:body => 'ほんぶんだよ！',
-			:photo => 'http://example.com/foo.jpg',
-		},
-		{
-			# レスエントリ用
-			:body => 'ほんぶんだよ２！',
-			:photo => 'http://example.com/baa.jpg',
-		}
-	][no]
-end
-
 describe 'Massr::Entry' do
 	before do
 		Massr::Entry.collection.remove
-		@user1 = Massr::User.new
-		@user2 = Massr::User.new
 	end
 
 	describe '#update_entry' do
 		before do
-			@entry1_1 = Massr::Entry.new.update_entry( prototype_entry(0) , {:user => @user1} )
+			@user = Massr::User.create_by_registration_form( prototype_user(0) )
+			@entry1_1 = Massr::Entry.new.update_entry( prototype_entry(0,@user) )
 		end
 		subject {@entry1_1}
 		
 		context 'Entryが正常に登録できているか' do
-			its(:body)  {should eq(prototype_entry(0)[:body]) }
-			its(:photo) {should eq(prototype_entry(0)[:photo]) }
-			its(:user)  {should eq(@user1) }
+			its(:body)  {should eq(prototype_entry(0,@user)[:body]) }
+			its(:photo) {should eq(prototype_entry(0,@user)[:photo]) }
+			its(:user)  {should eq(@user) }
 			its(:res)   {should raise_error(NoMethodError) }
 		end
 	end
 		
 	describe '#response_entry' do
 		before do
-			@entry2_1 = Massr::Entry.new.update_entry( prototype_entry(0) , {:user => @user1} )
+			@user = Massr::User.create_by_registration_form( prototype_user(1) )
+			@entry2_1 = Massr::Entry.new.update_entry( prototype_entry(0,@user) )
 			req = {
-				:body => prototype_entry(1)[:body],
-				:photo => prototype_entry(1)[:photo],
-				:res_id => @entry2_1._id
+				:body   => prototype_entry(1,@user)[:body],
+				:photo  => prototype_entry(1,@user)[:photo],
+				:res_id => @entry2_1._id,
+				:user   => prototype_entry(1,@user)[:user]
 			}
-			@entry2_2 = Massr::Entry.new.update_entry( req , {:user => @user2} )
+			@entry2_2 = Massr::Entry.new.update_entry( req )
 		end
 
 		subject {@entry2_2}
 		context 'レスポンスEntryが正常に登録できているか' do
-			its(:body)  {should eq(prototype_entry(1)[:body]) }
-			its(:photo) {should eq(prototype_entry(1)[:photo]) }
-			its(:user)  {should eq(@user2) }
+			its(:body)  {should eq(prototype_entry(1,@user)[:body]) }
+			its(:photo) {should eq(prototype_entry(1,@user)[:photo]) }
+			its(:user)  {should eq(@user) }
 			its(:res)   {should eq(@entry2_1) }
 		end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,40 @@ RSpec.configure do |config|
 		MongoMapper.database.collections.each {|collection| collection.remove}
 	end
 end
+
+def prototype_user(no)
+	[
+		{
+			:massr_id => 'wasamas',
+			:twitter_id => '1234567',
+			:twitter_icon_url => 'http://example.com/foo1.png',
+			:name => 'わさます',
+			:email => 'wasamas@example.com',
+		},
+		{
+			:massr_id => 'wasamas2',
+			:twitter_id => '7654321',
+			:twitter_icon_url => 'http://example.com/foo2.png',
+			:name => 'わさます2',
+			:email => 'wasamas2@example.com',
+		}
+	][no]
+end
+
+def prototype_entry(no,user)
+	[
+		{
+			# 元エントリ用
+			:body  => 'ほんぶんだよ！',
+			:photo => 'http://example.com/foo.jpg',
+			:user  =>  user
+		},
+		{
+			# レスエントリ用
+			:body  => 'ほんぶんだよ２！',
+			:photo => 'http://example.com/baa.jpg',
+			:user  => user
+		}
+	][no]
+end
+
