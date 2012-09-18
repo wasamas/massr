1be415c6b525ca9533afc898c67163eb87166d1d
TADA Tadashi <t@tdtds.jp>
2012-09-18 19:39:31 +0900

    テストの準備をそれぞれのcontextごとに実施するように変更

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,35 +1,46 @@
 # -*- coding: utf-8 -*-
 require 'spec_helper'
 require 'massr/models/user'
-
+require 'massr/models/statement'
 
 describe 'Massr::User' do
-	before do
-		Massr::User.collection.remove
-	end
-
 	describe '.create_by_registration_form' do
-		before do
-			@user = Massr::User.create_by_registration_form( prototype_user(0) )
-		end
-		subject{ @user }
+		context '最初の新規ユーザ登録が正常にできているか' do
+			before :all do
+				Massr::User.collection.remove
+				@user = Massr::User.create_by_registration_form( prototype_user(0) )
+			end
+			subject{ @user }
 
-		context '新規ユーザ登録が正常にできているか' do
 			its(:massr_id) { should eq(prototype_user(0)[:massr_id]) }
 			its(:twitter_id) { should eq(prototype_user(0)[:twitter_id]) }
 			its(:twitter_icon_url) { should eq(prototype_user(0)[:twitter_icon_url]) }
 			its(:name) { should eq(prototype_user(0)[:name]) }
 			its(:email) { should eq(prototype_user(0)[:email]) }
+			its(:status) { should eq(0) }
+		end
+
+		context '2番目の新規ユーザ登録が正常にできているか' do
+			before :all do
+				Massr::User.collection.remove
+				Massr::User.create_by_registration_form( prototype_user(0) )
+				@user = Massr::User.create_by_registration_form( prototype_user(1) )
+			end
+			subject{ @user }
+
+			its(:massr_id) { should eq(prototype_user(1)[:massr_id]) }
+			its(:twitter_id) { should eq(prototype_user(1)[:twitter_id]) }
+			its(:twitter_icon_url) { should eq(prototype_user(1)[:twitter_icon_url]) }
+			its(:name) { should eq(prototype_user(1)[:name]) }
+			its(:email) { should eq(prototype_user(1)[:email]) }
 		end
 	end
 
 	describe '#update' do
-		before do
-			Massr::User.create_by_registration_form( prototype_user(0) )
-		end
-
 		context 'すべての属性を指定して更新する' do
 			before :all do
+				Massr::User.collection.remove
+				Massr::User.create_by_registration_form( prototype_user(0) )
 				@user = Massr::User.find_by_twitter_id(prototype_user(0)[:twitter_id])
 				@user.update_profile(prototype_user(1))
 			end
@@ -43,8 +54,5 @@ describe 'Massr::User' do
 			# massr_idを変えてはいけない
 			its(:massr_id) { should eq(prototype_user(0)[:massr_id]) }
 		end
-	end
-
-	after do
 	end
 end
