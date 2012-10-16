97d652a2a5af6524ff642a4bd6de6f2571d63791
Echos <echo.orz@gmail.com>
2012-10-16 23:29:16 +0900

    routes/searchのjson返却対応 #105

--- routes/search.rb
+++ routes/search.rb
@@ -10,25 +10,35 @@
 
 module Massr
 	class App < Sinatra::Base
+		before '/search*' do
+			@q = params[:q].strip
+			@total = total_page({:body => /#{@q}/})
+			@page = current_page
+		end
+
+		get '/search.json' do
+			[].tap {|a|
+				Statement.get_statements(@page,{:body => /#{@q}/}).each do |statement|
+					a << statement.to_hash
+				end
+			}.to_json
+		end
+
 		get '/search' do
-			q = params[:q].strip
-			if q.size == 0 then
+			if @q.size == 0 then
 				redirect '/'
 				return
 			end
-			if q != params[:q] then
-				redirect '/search?q=' + q
+			if @q != params[:q] then
+				redirect '/search?q=' + @q
 				return
 			end
 
-			total = total_page({:body => /#{q}/})
-			page = current_page
-
 			haml :index , :locals => {
-				:page => page, 
-				:statements => Statement.get_statements(page,{:body => /#{q}/}),
-				:q => q,
-				:total_page => total}
+				:page => @page, 
+				:statements => Statement.get_statements(@page,{:body => /#{@q}/}),
+				:q => @q,
+				:total_page => @total}
 		end
 	end
 end
