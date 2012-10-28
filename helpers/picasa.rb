fea5d55a3f8976c9ca3f67cc00247007ab3c66fc
pocketp <pocketp123@gmail.com>
2012-10-28 14:07:13 +0900

    #108 とりあえずimage/*が無い場合はファイル名から拡張子を取って、それでもなければ空文字列にするようにした。

--- helpers/picasa.rb
+++ helpers/picasa.rb
@@ -10,6 +10,20 @@
 module Massr
 	class App < Sinatra::Base
 		helpers do
+			def specify_content_type(head)
+				image_type = head.scan(/(image\/\w+)/)
+				if image_type.length > 0
+					return image_type[0][0]
+				end
+				# octet-stream
+				ext = head.scan(/filename=\"[^\"]*\.([^\"\.]+)\"/)
+				if ext.length > 0
+					return 'image/' + ext[0][0].downcase
+				end
+				# not found
+				return ''
+			end
+
 			def picasa_client
 				@picasa_client ||= Massr::Plugin::Picasa.new
 			end
@@ -18,7 +32,7 @@ module Massr
 				return nil unless photo_info && photo_info[:tempfile]
 
 				path = photo_info[:tempfile].to_path || ''
-				content_type = photo_info[:head].scan(/(image\/\w+)/)[0][0] || ''
+				content_type = specify_content_type(photo_info[:head])
 				return picasa_client.upload_file(path, content_type)
 			end
 		end
