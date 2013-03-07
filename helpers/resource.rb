b4409720a7b8b2f55b63371a69e79652068d37e3
TADA Tadashi <t@tdtds.jp>
2013-03-07 12:21:06 +0900

    リソースをsettings.jsonに追い出し #68

--- helpers/resource.rb
+++ helpers/resource.rb
@@ -10,81 +10,12 @@
 
 module Massr
 	class App < Sinatra::Base
-		helpers do
-			#
-			# カスタマイズ可能な文字列
-			#
-			def _like
-				'わかるわ'
-			end
+		SETTINGS = JSON.parse(open('public/settings.json', &:read))
 
-			def _unlike
-				'わからないわ'
-			end
-
-			def _delete
-				'削除'
-			end
-
-			def _clearres
-				'新着レスクリア'
-			end
-
-			def _res
-				'レス'
-			end
-
-			def _post
-				'投稿にょわー☆'
-			end
-
-			def _post_res
-				'レスるわ'
-			end
-
-			def _search
-				'みつけるわ'
-			end
-
-			def _menu
-				'メニュー'
-			end
-
-			def _member
-				'メンバ'
-			end
-
-			def _unprivilege_user
-				'管理者権限を剥奪する'
-			end
-
-			def _privilege_user
-				'管理者にする'
-			end
-
-			def _unauthorize_user
-				'認可を取り消す'
-			end
-
-			def _authorize_user
-				'認可する'
-			end
-
-			def _unauth_count(num)
-				"未認証ユーザが#{num}人います"
-			end
-
-			def _response_count(num)
-				"新着レスが#{num}個あります"
-			end
-
-			def _delete_button
-				'×'
-			end
-
-			def _user_info_update
-				"アプリケーション更新に伴い、ユーザ情報の再登録をします"
-			end
+		SETTINGS['local'].each do |key, value|
+			define_method("_#{key}"){|*args|
+				value.gsub(/%(\d+)/){args[$1.to_i - 1]}
+			}
 		end
 	end
 end
